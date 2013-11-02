295ba237c61c1d915764d359721550d8b5c4136d
Ruben Impens <ruben.impens@dotprojects.be>
2013-11-02 23:38:57 +0100

    Add placeholder to status body field.

--- sites/all/themes/custom/omega_music_blog/preprocess/preprocess.forms.php
+++ sites/all/themes/custom/omega_music_blog/preprocess/preprocess.forms.php
@@ -9,6 +9,12 @@
  * Implements hook_preprocess_textarea().
  */
 function omega_music_blog_preprocess_textarea(&$variables) {
+  $element = $variables['element'];
   // We don't want resizable textareas.
   $variables['element']['#resizable'] = FALSE;
+
+  // Add a placeholder to the status node field.
+  if (isset($element['#bundle']) && $element['#bundle'] == 'status' && $element['#field_name'] == 'body') {
+    $variables['element']['#attributes']['placeholder'] = music_extras_body_placeholder();
+  }
 }
