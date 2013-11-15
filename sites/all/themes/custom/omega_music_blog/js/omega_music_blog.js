af903aa02b570017e6d48e35e1e8fe5a49be941e
Ruben Impens <ruben.impens@dotprojects.be>
2013-11-16 00:02:37 +0100

    Theming update.

--- sites/all/themes/custom/omega_music_blog/js/omega_music_blog.js
+++ sites/all/themes/custom/omega_music_blog/js/omega_music_blog.js
@@ -3,10 +3,20 @@
     attach: function (context, settings) {
       // Expand status body field on focus.
       var bodyField = $('#block-formblock-status .field-name-field-body textarea', context);
+      var defaultValue = bodyField.val();
       bodyField.focus(function() {
         $(this).animate({'rows':'5'}, 100);
       }).blur(function() {
-        $(this).animate({'rows':'1'}, 100);
+        if ($(this).val() == defaultValue) {
+          $(this).animate({'rows':'1'}, 100);
+        }
+      });
+      // Form submit hover.
+      var submit = $('.form-submit');
+      submit.hover(function() {
+        $(this).animate({'backgroundColor': '#d1344e'}, 100);
+      }, function() {
+        $(this).animate({'backgroundColor': '#525252'}, 'fast');
       });
     }
   };
